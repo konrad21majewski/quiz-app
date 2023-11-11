f7c7736f9f5cd953c9cc4c6e9505b5fe1e890286
konrad21majewski <konrad21majewski@gmail.com>
2023-11-12 00:33:19 +0100

    graphql import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeacherModule } from './teacher/teacher.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TeacherModule],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }), TeacherModule,
+  ]
 })
-export class AppModule {}
+export class AppModule { }
