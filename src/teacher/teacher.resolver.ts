8b264c6af9cc5b68a408d9129959295e44f9f96a
konrad21majewski <konrad21majewski@gmail.com>
2023-11-22 23:00:43 +0100

    teacher first service and resolver

--- src/teacher/teacher.resolver.ts
+++ src/teacher/teacher.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+import { TeacherService } from './teacher.service';
+import { Teacher } from './teacher.entity';
 
-@Resolver()
-export class TeacherResolver {}
+
+@Resolver(of => Teacher)
+export class TeacherResolver {
+    constructor(private teacherService: TeacherService) { }
+
+    @Query(returns => [Teacher])
+    teachers(): Promise<Teacher[]> {
+        return this.teacherService.findAll();
+    }
+}
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Teacher } from './teacher.entity';
 
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+    async findAll(): Promise<Teacher[]> {
+        const teacher = new Teacher();
+        teacher.id = 1;
+        teacher.name = "Walter";
+
+        return [teacher];
+    }
+}
